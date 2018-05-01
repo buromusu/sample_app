ccc04d662031821aefef3a95281702ed178c0e14
山内　翔大 <buromusu@h24.45.240.10.31634.vlan.kuins.net>
2018-05-01 17:27:05 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  host = '10.240.45.24:3000' # ここをコピペすると失敗します。自分の環境に合わせてください。
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
