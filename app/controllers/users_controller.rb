c4795fbce991f645de10ed7c58fc45a90ac5a190
山内　翔大 <buromusu@h24.45.240.10.31634.vlan.kuins.net>
2018-05-01 16:51:22 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
-      log_in @user
-      # 保存の成功をここで扱う。
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
